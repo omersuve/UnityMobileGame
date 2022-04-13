9fa05a9515ec111ab6d56fc70fd1e45a610ad980
omersuve <omer@shopihq.com>
2022-04-13 16:43:29 +0200

    box collider added

--- UnityNFTGame/Assets/Scripts/PlayerMovement.cs
+++ UnityNFTGame/Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,19 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
+    private RaycastHit2D hit;
+    private BoxCollider2D boxCollider;
+
     public Rigidbody2D rb;
     public Camera cam;
 
     Vector2 movement;
     Vector2 mousePos;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
 
     private void Update()
     {
@@ -25,5 +33,17 @@ public class PlayerMovement : MonoBehaviour
         Vector2 lookDir = mousePos - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
         rb.rotation = angle;
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, movement.y), Mathf.Abs(movement.y * Time.deltaTime), LayerMask.GetMask("Player", "Collider"));
+        if (hit.collider == null)
+        {
+            transform.Translate(0, movement.y * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(movement.x, 0), Mathf.Abs(movement.x * Time.deltaTime), LayerMask.GetMask("Player", "Collider"));
+        if (hit.collider == null)
+        {
+            transform.Translate(movement.x * Time.deltaTime, 0, 0);
+        }
     }
 }
