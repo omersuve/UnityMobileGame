bc62ce4040dde81f9f761816618d63cbf26fda1f
omersuve <omer@shopihq.com>
2022-05-01 00:55:05 +0200

    logged in user different color

--- UnityNFTGame/Assets/Scripts/PlayfabScoreboard.cs
+++ UnityNFTGame/Assets/Scripts/PlayfabScoreboard.cs
@@ -14,11 +14,14 @@ public class PlayfabScoreboard : MonoBehaviour
     public Transform rowsParent;
     public Button getBackButton;
 
+    string loggedInPlayfabID;
+
     private void Start()
     {
         Button btnGetBack = getBackButton.GetComponent<Button>();
         btnGetBack.onClick.AddListener(GetBack);
         GetLeaderboard();
+        GetAccountInfo();
     }
 
     public void GetBack()
@@ -54,6 +57,13 @@ public class PlayfabScoreboard : MonoBehaviour
             texts[1].text = item.DisplayName;
             texts[2].text = item.StatValue.ToString();
 
+            if(item.PlayFabId == loggedInPlayfabID)
+            {
+                texts[0].color = Color.cyan;
+                texts[1].color = Color.cyan;
+                texts[2].color = Color.cyan;
+            }
+
             Debug.Log(string.Format("PLACE: {0} | ID: {1} | VALUE: {2}",
                 item.Position, item.PlayFabId, item.StatValue));
 
@@ -64,5 +74,17 @@ public class PlayfabScoreboard : MonoBehaviour
     {
         Debug.Log(error.ErrorMessage);
         Debug.Log(error.GenerateErrorReport());
+    }
+
+    void GetAccountInfo()
+    {
+        GetAccountInfoRequest request = new GetAccountInfoRequest();
+        PlayFabClientAPI.GetAccountInfo(request, OnGetAccount, OnError);
+    }
+
+
+    void OnGetAccount(GetAccountInfoResult result)
+    {
+        loggedInPlayfabID = result.AccountInfo.PlayFabId;
     }
 }
